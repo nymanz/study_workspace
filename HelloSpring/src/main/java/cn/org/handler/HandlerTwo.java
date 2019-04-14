adfd44053e6a78b67439251198b7e36d8428b1f4
zhaoqianze <980299466@qq.com>
2019-04-14 14:46:00 +0800

    delete blank

--- HelloSpring/src/main/java/cn/org/handler/HandlerTwo.java
+++ HelloSpring/src/main/java/cn/org/handler/HandlerTwo.java
@@ -3,7 +3,6 @@ import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
 
-
 public class HandlerTwo extends IoHandlerAdapter {
 	 @Override
 	    public void messageReceived(IoSession session, Object message)
